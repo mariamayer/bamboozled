60e7bd8f4bbc4194a5f6744d8f26b8f4d4bfcf2c
Christiana Koudigkeli <ckoudigkeli@gmail.com>
2017-07-14 14:44:59 +0200

    User model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const userSchema = new Schema({
     email           : { type: String, required: true },
     name            : { type: String, default: '' },
     password        : { type: String, required: true },
-    // timestamps      : { createdAt: "created_at", updatedAt: "updated_at" },
     answersPosted   : { type: Number, default: 0 },
     questionsPosted : { type: Number, default: 0 },
     age             : { type: Number, default: 18 },
@@ -13,6 +12,8 @@ const userSchema = new Schema({
     role            : { type: String, default: '' },
     bio             : { type: String, default: '' },
     avatar          : { type: String, default: "" }
+}, {
+    timestamps      : { createdAt: "created_at", updatedAt: "updated_at" },
 });
 
 const User = mongoose.model("User", userSchema);
