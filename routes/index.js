ac0f32dbbadf092ef71071f4de84fcceb350496c
Maria Mayer <mayermaru@gmail.com>
2017-07-16 13:05:12 +0200

    Bootstrap installation, initial home layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const Post = require('../models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bamboozled' });
+  Post.find({}, (err, posts) => {
+    if (err) { return next(err) }
+    res.render('index', {
+      posts: posts
+    });
+  });
 });
 
 module.exports = router;
